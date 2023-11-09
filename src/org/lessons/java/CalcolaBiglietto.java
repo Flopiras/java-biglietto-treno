91bce1b846832b8837ce9f1136b1154b96d866ee
Flopiras <Flopiras@yahoo.it>
2023-11-09 16:54:36 +0100

    asked user informations

--- src/org/lessons/java/CalcolaBiglietto.java
+++ src/org/lessons/java/CalcolaBiglietto.java
@@ -1,5 +1,25 @@
 package org.lessons.java;
 
-public class CalcolaBiglietto {
+//importo Scanner
+import java.util.Scanner;
 
+public class CalcolaBiglietto {
+	public static void main(String[] args) {
+		
+		//apro lo Scanner
+		Scanner in = new Scanner(System.in);
+		
+		//chiedo all'utente quanti chilometri intende fare
+		System.out.println("Quanti chilometri si vogliono percorrere? (digitare la cifra)");
+		String strKilometers = in.nextLine();
+		int intKilometers = Integer.valueOf(strKilometers);
+		
+		//chiedo all'utente quanti anni ha il passeggero
+			System.out.println("Quanti anni ha il passeggero? (digitare la cifra)");
+			String strAge = in.nextLine();
+			int intAge = Integer.valueOf(strAge);
+			
+		//chiudo lo scanner
+			in.close();
+	}
 }
